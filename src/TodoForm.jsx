5ebfbe3ddcecd67560b9441a48248f9964a00c56
franklin-05 <franklin97salazar@gmail.com>
2025-12-21 20:43:43 -0500

    updates and states complete

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,13 +1,28 @@
-function TodoForm() {
+function TodoForm({ onAddTodo }) {
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    
+    const title = event.target.title.value;
+    
+    onAddTodo(title);
+
+    event.target.title.value = "";
+  }
+
   return (
     <div>
-   <form>
-    <label htmlFor="todo-input">Todo:</label>
-      <input type="text" />
-      <button type="submit">Add Todo</button>
-    </form>
+      <form onSubmit={handleAddTodo}>
+        <label htmlFor="todo-input">Todo:</label>
+        <input 
+          type="text"
+          name="title" 
+          id="todo-input"
+        />
+        <button type="submit">Add Todo</button>
+      </form>
     </div>
-   
   );
 }
+
 export default TodoForm;
